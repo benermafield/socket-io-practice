dcabb004d076926cc32948caaed186a016029d42
Andrew Benfield <andymania@gmail.com>
2015-07-14 19:19:57 +0800

    Extracted the scripts to their own files. Added support for nicknames and disallowed multiple users to have the same nickname.

---
+++ js/index.js
@@ -0,0 +1,61 @@
+var socket = io();
+
+var nickname = "";
+
+$('form#messagebox').submit(function() {
+    message = $('#m').val();
+    socket.emit('chat message', {
+        text: message,
+        nick: nickname
+    });
+    $('#m').val('');
+    return false;
+});
+
+$('form#nicknamebox').submit(function() {
+    nickname = $('#nickname').val();
+    socket.emit('login', nickname);
+    return false;
+});
+
+socket.on('chat message', function(msg) {
+    $('#messages').append($('<li>').text(msg.nick + ": " + msg.text));
+});
+
+socket.on('user connected', function() {
+    $('#messages').append($('<li>', {
+        class: 'info'
+    }).text('A user has connected!'));
+});
+
+socket.on('login', function(nick) {
+    $('#messages').append($('<li>', {
+        class: 'info'
+    }).text(nick + ' has joined the chat!'));
+});
+
+socket.on('user disconnected', function(nick) {
+    if (nick != "") {
+        $('#messages').append($('<li>', {
+            class: 'info'
+        }).text(nick + ' has disconnected!'));
+    } else {
+        $('#messages').append($('<li>', {
+            class: 'info'
+        }).text('A user has disconnected!'));
+    }
+});
+
+socket.on('login success', function() {
+    $('form#nicknamebox').hide();
+    $('form#messagebox').show();
+});
+
+socket.on('nickname exists', function() {
+    $('#nickname').attr('placeholder', 'Someone has that nickname... Another!')
+    $('#nickname').val('');
+});
+
+$(document).ready(function() {
+    $('form#nicknamebox').show();
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var users = {};
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css/index.css', function(req, res) {
+	res.sendFile(__dirname + '/css/index.css');
+});
+
+app.get('/js/index.js', function(req, res) {
+	res.sendFile(__dirname + '/js/index.js');
+});
+
+io.on('connection', function(socket) {
+	io.emit('user connected');
+	socket.nickname = "";
+
+	socket.on('disconnect', function() {
+		if (socket.nickname != "") {
+			delete users[socket.nickname];
+		}
+		io.emit('user disconnected', socket.nickname);
+	});
+
+	socket.on('login', function(nick) {
+		if (nick in users) {
+			socket.emit('nickname exists');
+		} else {
+			socket.nickname = nick;
+			users[socket.nickname] = socket.id;
+			socket.emit('login success');
+			io.emit('login', nick);
+		}
+	});
+
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
